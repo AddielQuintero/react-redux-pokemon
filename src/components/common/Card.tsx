2d0a264e63fe215a7965117e5118e67f34e1389c
atamayo <atamayoquintero@gmail.com>
2023-05-28 16:54:26 -0500

    feat: add button, card and input component

---
+++ src/components/common/Card.tsx
@@ -0,0 +1,43 @@
+import { CardLinkProps, CardProps } from '@types'
+import { Link } from 'react-router-dom'
+
+export const CustomCard = (props: CardProps) => {
+  // const containerStyle = { ...props.style, border: `1px solid ${props.style?.color}` }
+
+  return (
+    <>
+      <div className={props.className} style={props.style}>
+        <div style={props.style} className={props.classContentImg}>
+          {props.classImg && <img className={props.classImg} src={props.src} alt="" />}
+        </div>
+        {props.classTitle && <h2 className={props.classTitle}>{props.title}</h2>}
+        {props.classSubTitle && <p className={props.classSubTitle}>{props.subTitle}</p>}
+        {props.classContentLink && (
+          <div className={props.classContentLink}>
+            {props.links &&
+              props.links.map((item: CardLinkProps) => (
+                <Link
+                  to={item.to}
+                  className={item.classLink}
+                  style={{
+                    backgroundColor: `${item.styleLink.color}50`,
+                    color: `${item.styleLink.color}`,
+                  }}
+                  key={item.label}
+                >
+                  {item.label}
+                </Link>
+              ))}
+          </div>
+        )}
+        {props.hr && <hr />}
+        {props.classBody && (
+          <div className={props.classBody}>
+            <h3 className={props.classBodyTitle}>{props.bodyTitle}</h3>
+            <p className={props.classBodyDescription}>{props.bodyDescription}</p>
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export * from './Colors';
+export * from './Pokemon';
+export * from './Redux';
+export * from './common';
+export * from './index';
