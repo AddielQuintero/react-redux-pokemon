6ad89780d9b5b96b8c6386a3dfda931054b90392
atamayo <atamayoquintero@gmail.com>
2023-06-07 19:05:56 -0500

    fix: improve button and card component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -3,6 +3,7 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
 export const CustomButton = (props: ButtonProps): JSX.Element => {
   return (
     <button
+      style={props.style}
       type={props.type}
       className={props.className}
       disabled={props.disabled}
